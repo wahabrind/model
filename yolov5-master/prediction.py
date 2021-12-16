102fcebbf9a1560c301828f2df084be3542367c5
abdul-wahab-rind <abdul.wahab@jogo.ai>
2021-12-16 20:18:51 +0500

    bug fixes

--- yolov5-master/prediction.py
+++ yolov5-master/prediction.py
@@ -2,16 +2,16 @@ from person_detection_main import Model
 import cv2
 
 
-cap = cv2.VideoCapture('vid2.avi')
-obj  = Model()
+cap = cv2.VideoCapture('vid.mp4')
+obj = Model()
 
 while True:
-    ret , frame = cap.read()
-    dict , img  = obj.run(frame)
-    
-    cv2.imshow('a' , img)
+    ret, frame = cap.read()
+    dict, img = obj.run(frame)
+
+    cv2.imshow('a', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
 
-    print(dict) 
+    print(dict)
